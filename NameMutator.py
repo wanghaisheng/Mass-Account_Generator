a39fcf29667f528d701ceff2f01f56c80dacaf25
HeisenBerg? <admin@tiktokastudio.com>
2024-07-18 10:19:08 +0800

    Create NameMutator.py

---
+++ NameMutator.py
@@ -0,0 +1,138 @@
+class NameMutator():
+    """
+    This class handles all name mutations.
+
+    Init with a raw name, and then call the individual functions to return a mutation.
+    """
+    def __init__(self, name):
+        self.name = self.clean_name(name)
+        self.name = self.split_name(self.name)
+
+    @staticmethod
+    def clean_name(name):
+        """
+        Removes common punctuation.
+
+        LinkedIn users tend to add credentials to their names to look special.
+        This function is based on what I have seen in large searches, and attempts
+        to remove them.
+        """
+        # Lower-case everything to make it easier to de-duplicate.
+        name = name.lower()
+
+        # Use case for tool is mostly standard English, try to standardize common non-English
+        # characters.
+        name = re.sub("[àáâãäå]", 'a', name)
+        name = re.sub("[èéêë]", 'e', name)
+        name = re.sub("[ìíîï]", 'i', name)
+        name = re.sub("[òóôõö]", 'o', name)
+        name = re.sub("[ùúûü]", 'u', name)
+        name = re.sub("[ýÿ]", 'y', name)
+        name = re.sub("[ß]", 'ss', name)
+        name = re.sub("[ñ]", 'n', name)
+
+        # Get rid of all things in parenthesis. Lots of people put various credentials, etc
+        name = re.sub(r'\([^()]*\)', '', name)
+
+        # The lines below basically trash anything weird left over.
+        # A lot of users have funny things in their names, like () or ''
+        # People like to feel special, I guess.
+        allowed_chars = re.compile('[^a-zA-Z -]')
+        name = allowed_chars.sub('', name)
+
+        # Next, we get rid of common titles. Thanks ChatGPT for the help.
+        titles = ['mr', 'miss', 'mrs', 'phd', 'prof', 'professor', 'md', 'dr', 'mba']
+        pattern = "\\b(" + "|".join(titles) + ")\\b"
+        name = re.sub(pattern, '', name)
+
+        # The line below tries to consolidate white space between words
+        # and get rid of leading/trailing spaces.
+        name = re.sub(r'\s+', ' ', name).strip()
+
+        return name
+
+    @staticmethod
+    def split_name(name):
+        """
+        Takes a name (string) and returns a list of individual name-parts (dict).
+
+        Some people have funny names. We assume the most important names are:
+        first name, last name, and the name right before the last name (if they have one)
+        """
+        # Split on spaces and dashes (included repeated)
+        parsed = re.split(r'[\s-]+', name)
+
+        # Iterate and remove empty strings
+        parsed = [part for part in parsed if part]
+
+        # Discard people without at least a first and last name
+        if len(parsed) < 2:
+            return None
+
+        if len(parsed) > 2:
+            split_name = {'first': parsed[0], 'second': parsed[-2], 'last': parsed[-1]}
+        else:
+            split_name = {'first': parsed[0], 'second': '', 'last': parsed[-1]}
+
+        # Final sanity check to not proceed without first and last name
+        if not split_name['first'] or not split_name['last']:
+            return None
+
+        return split_name
+
+    def f_last(self):
+        """jsmith"""
+        names = set()
+        names.add(self.name['first'][0] + self.name['last'])
+
+        if self.name['second']:
+            names.add(self.name['first'][0] + self.name['second'])
+
+        return names
+
+    def f_dot_last(self):
+        """j.smith"""
+        names = set()
+        names.add(self.name['first'][0] + '.' + self.name['last'])
+
+        if self.name['second']:
+            names.add(self.name['first'][0] + '.' + self.name['second'])
+
+        return names
+
+    def last_f(self):
+        """smithj"""
+        names = set()
+        names.add(self.name['last'] + self.name['first'][0])
+
+        if self.name['second']:
+            names.add(self.name['second'] + self.name['first'][0])
+
+        return names
+
+    def first_dot_last(self):
+        """john.smith"""
+        names = set()
+        names.add(self.name['first'] + '.' + self.name['last'])
+
+        if self.name['second']:
+            names.add(self.name['first'] + '.' + self.name['second'])
+
+        return names
+
+    def first_l(self):
+        """johns"""
+        names = set()
+        names.add(self.name['first'] + self.name['last'][0])
+
+        if self.name['second']:
+            names.add(self.name['first'] + self.name['second'][0])
+
+        return names
+
+    def first(self):
+        """john"""
+        names = set()
+        names.add(self.name['first'])
+
+        return names
